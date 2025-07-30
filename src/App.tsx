ea9a24d105fbf43cd86a544e52c684f4b2099c3e
wasabina67 <wasabina67@gmail.com>
2025-07-30 20:05:20 +0900

    Update src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,43 @@
+import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
 // import { useState } from 'react'
 import './App.css'
+
+const queryClient = new QueryClient()
+
+function Example() {
+  const { isPending, error, data, isFetching } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: async () => {
+      const response = await fetch(
+        'https://api.github.com/repos/TanStack/query',
+      )
+      return await response.json()
+    },
+  })
+
+  if (isPending) return 'Loading...'
+
+  if (error) return 'An error has occurred: ' + error.message
+
+  return (
+    <div>
+      <h1>{data.full_name}</h1>
+      <p>{data.description}</p>
+      <strong>👀 {data.subscribers_count}</strong>{' '}
+      <strong>✨ {data.stargazers_count}</strong>{' '}
+      <strong>🍴 {data.forks_count}</strong>
+      <div>{isFetching ? 'Updating...' : ''}</div>
+    </div>
+  )
+}
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>test</h1>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Example />
+    </QueryClientProvider>
   )
 }
 
